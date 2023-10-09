3f3b677f04d5dbd8e70f301aaf8b300e19cec4a2
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-10-09 08:44:21 +0000

    Create ci-audit-logging.ts

---
+++ lib/auditing/ci-audit-logging.ts
@@ -0,0 +1,28 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The Distronode Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+import { Duration, Stack } from 'aws-cdk-lib';
+import { Bucket, BucketAccessControl, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
+
+export class CiAuditLogging {
+  public readonly bucket: Bucket;
+
+  constructor(stack: Stack) {
+    this.bucket = new Bucket(stack, 'jenkinsAuditBucket', {
+      serverAccessLogsBucket: this.bucket,
+      serverAccessLogsPrefix: 's3AccessLogs/',
+      accessControl: BucketAccessControl.LOG_DELIVERY_WRITE,
+      objectOwnership: ObjectOwnership.BUCKET_OWNER_PREFERRED,
+      lifecycleRules: [
+        {
+          expiration: Duration.days(3650),
+        },
+      ],
+    });
+  }
+}
